c921a72178294249d319a2ff99c2cd8bcc2abb3d
Alejandro Londoño <86858494+slydragonn@users.noreply.github.com>
2022-07-16 14:47:39 -0500

    Update mark-writer.js

--- pages/projects/mark-writer.js
+++ pages/projects/mark-writer.js
@@ -7,7 +7,7 @@ const Hello = () => {
     <Layout>
       <PageLayout title="Mark Writer">
         <ProjectTemplate
-          image='/assets/editor.png'
+          image='../public/assets/editor.png'
           summary='Mark Writer es un simple y sencillo Editor Web de Markdown,
            con el cual puedes visualizar ccódigo Markdown a HTML, ademas puedes copiar el código Markdown y HTML
             o exportarlos a sus respectivos archivos.'
