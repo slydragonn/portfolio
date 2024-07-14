6029dca04f4f089df93958ce93642ab867267ca1
slydragonn <alogo18m@gmail.com>
2024-07-14 17:52:39 -0500

    fix: site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://',
+	site: 'https://slydragonn.netlify.com',
 	integrations: [mdx(), sitemap()],
 });
